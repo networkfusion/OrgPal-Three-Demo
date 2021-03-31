390901005827452612ae6f6b4164845d6b49034d
Robin Jones <robin@networkfusion.co.uk>
2021-03-31 20:54:07 +0100

    Add class for LufftVentus

---
+++ OrgPalThreeDemo/Peripherals/LufftVentus.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace OrgPalThreeDemo.Peripherals
+{
+    public class LufftVentus
+    {
+
+        //Connection type
+        // RS485-HalfDuplex
+        // RS485-FullDuplex
+        // SDI
+        // MODBUS
+
+        //RS485 119200 8N1 - Default
+
+        //Protocol
+        //UMB Binary
+        //UMB ASCII - Depricated
+        //NMEA
+        //SDI-12
+        //MODBUS RTU
+        //MODBUS ASCII
+
+        //public enum Status
+        //{
+        //    VoltageUpper = 4006, //ThresholdExceeded above 28VDC
+        //    VoltageLower = 4007, //ThresholdExceeded below 20VDC
+        //    HeatingLower = 4997,
+        //    HeatingUpper = 4998
+        //}
+
+        //Properties
+        //public int WindSpeed { get; set; }
+        //public int WindDirection { get; set; }
+        //public int WindMeasurementQuality { get; set; }
+        //public int VirtualTemprature { get; set; }
+        //public int AirPressure { get; set; }
+        //public int AirDensity { get; set; }
+        //public int Heating { get; set; }
+        //public int CurrentStatus { get; set; }
+
+        ////public int Humidity { get; set; } //This needs to be sent to the sensor for working out air density.
+
+
+        //private void DecodeNmeaMessage
+        //{
+
+        //}
+
+        //private void DecodeUmbBinaryMessage
+        //{
+
+        //}
+
+        //private void DecodeUmbASCIIMessage
+        //{
+
+        //}
+
+
+        //private class UmbBinaryMessage
+        //{
+
+        //}
+
+        //private class NmeaMessage
+        //{
+
+        //}
+    }
+}
