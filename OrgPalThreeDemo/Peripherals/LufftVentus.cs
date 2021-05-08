31a871c36c546a417be2eef65de0e55adf35d711
Robin Jones <robin@networkfusion.co.uk>
2021-05-08 18:30:11 +0100

    Work on lufft message encoding.

--- OrgPalThreeDemo/Peripherals/LufftVentus.cs
+++ OrgPalThreeDemo/Peripherals/LufftVentus.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace OrgPalThreeDemo.Peripherals
 {
@@ -12,16 +13,18 @@ namespace OrgPalThreeDemo.Peripherals
             const byte CHAR_ETX = 0x03;
             const byte CHAR_EOT = 0x04;
 
+            const byte UMB_PROTOCOL_VERSION_V1_0 = 0x10; // V1.0
+
+            const short DEFAULT_UMB_MASTER_DEVICE = 0x01 + 0x80; // From (device & class (Master unit))
+            const byte DEFAULT_UMB_CMD_LENGTH = 0x04; // This should remain consistant with UMB_PROTOCOL_VERSION V1.0
+            const byte DEFAULT_UMB_REQUEST_TYPE = 0x23; // Tjis is the only know type... Defined as: the command for the online data request
+
             public class UmbBinaryMessage
             {
                 //Message structure...
                 // SOH | VER | TO (device ID | class) | FROM (Master unit) | len | STX | CMD | VER | CHANNEL | ETX | CHKSUM  | EOT      
                 // 01h | 10h | 01h 80h                | 01h F0h            | 04h | 02h | 23h | 10h | 64h 00h | 03h | 0Bh 54h | 04h 'this is a request for the temperature on channel 64, so need to deal with the checksum
-                const byte UMB_PROTOCOL_VERSION_V1_0 = 0x10; // V1.0
-                
-                const short DEFAULT_UMB_MASTER_DEVICE = 0x01 + 0x80; // From (device & class (Master unit))
-                const byte DEFAULT_UMB_CMD_LENGTH = 0x04; // This should remain consistant with UMB_PROTOCOL_VERSION V1.0
-                const byte DEFAULT_UMB_REQUEST_TYPE = 0x23; // Defined as: the command for the online data request
+
 
                 public enum SensorType : short
                 {
@@ -36,32 +39,39 @@ namespace OrgPalThreeDemo.Peripherals
                     WIND_GUST_3S_KTS = 458, // 0xCA + 0x10; //dec 458 (little endian)
             }
 
-                public byte ProtocolVersion { get; set; }
-                public short ToAddress { get; set; } //this should allow different sensors!
-                public short FromAddress { get; set; }
-                public byte Command { get; set; }
-                public short Channel { get; set; }
+                public byte ProtocolVersion { get; set; } = UMB_PROTOCOL_VERSION_V1_0;
+                public SensorType ToAddress { get; set; } = SensorType.VENTUS; //this should allow different sensors!
+                public short FromAddress { get; set; } = DEFAULT_UMB_MASTER_DEVICE;
+                public byte Command { get; set; } = DEFAULT_UMB_REQUEST_TYPE;
+                public ParameterChannelType Channel { get; set; } = ParameterChannelType.WIND_AIR_TEMP;
+
+                public byte[] EncodeMessage()
+                {
+
+                    const short UMB_CHKSUM_TEST = 0x0B + 0x54; //Currently only for "ParameterChannelType.WIND_AIR_TEMP"
+                    var BytesToSend = new byte[] {
+                        CHAR_SOH,
+                        ProtocolVersion,
+                        (byte)((short)ToAddress & 255), //the "TO" address (high byte)
+                        (byte)((short)ToAddress >> 8), //the "TO" address (low byte)
+                        (byte)(FromAddress & 255), //UMB_MASTER_DEVICE, (high byte)
+                        (byte)(FromAddress >> 8), //UMB_MASTER_DEVICE, (low byte)
+                        DEFAULT_UMB_CMD_LENGTH,
+                        CHAR_STX,
+                        Command,
+                        ProtocolVersion,
+                        (byte)((short)Channel & 255), //high byte
+                        (byte)((short)Channel >> 8), //low byte
+                        CHAR_ETX,
+                        (UMB_CHKSUM_TEST & 255), //high byte
+                        (UMB_CHKSUM_TEST >> 8), //low byte
+                        CHAR_EOT 
+                    };
+
+                    Debug.WriteLine($"BytesToSend: {BytesToSend}");
+                    return BytesToSend;
+                }
             }
-
-            //public void EncodeMessage()
-            //{
-
-            //    const short UMB_CHKSUM_TEST = 0x0B + 0x54; //Currently only for "ParameterChannelType.WIND_AIR_TEMP"
-            //    //TODO: BytesToSend will need to be in a function or a Variable
-            //    var BytesToSend = new byte[] { 
-            //    CHAR_SOH,
-            //    DEFAULT_UMB_PROTOCOL_VERSION_V1_0,
-            //    SensorType.VENTUS, //the "TO" address
-            //    FromAddress, //UMB_MASTER_DEVICE, 
-            //    DEFAULT_UMB_CMD_LENGTH, 
-            //    CHAR_STX, 
-            //    UMB_REQUEST_TYPE,
-            //    DEFAULT_UMB_PROTOCOL_VERSION_V1_0,
-            //    ParameterChannelType.WIND_AIR_TEMP, 
-            //    CHAR_ETX, 
-            //    UMB_CHKSUM_TEST, 
-            //    CHAR_EOT;
-            //}
 
         }
 
