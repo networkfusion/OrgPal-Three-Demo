6c6c58c6d99e2af1bb70ee7ac7a6959d854bd185
Robin Jones <robin@networkfusion.co.uk>
2021-08-20 23:29:38 +0100

    Start thinking about unit tests!

---
+++ OrgPalThreeDemo/AwsIoTtestLib/Tests/program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using nanoFramework.Aws.IoTCore;
+
+namespace nanoFramework.Aws.Tests
+{
+    public class program //TODO: finish tests!
+    {
+        MqttConnectionClient _awsMqttClient;
+
+        void Setup() {
+            //TODO: should this use the AWS CLI to provision an instance, or at least reccomend one is already setup???
+
+            //X509Certificate caCert = new X509Certificate(AwsIoTCoreDefaultRootCA);
+            //X509Certificate2 clientCert = new X509Certificate2(AwsIoTCoreUriInstance.ClientRsaSha256Crt, AwsIoTCoreUriInstance.ClientRsaKey, ""); //make sure to add a correct pfx certificate
+
+            //_awsMqttClient = new MqttConnectionClient("AwsIoTCoreUriInstance", "nanoFrameworkTestRunner", clientCert, nanoFramework.M2Mqtt.Messages.MqttQoSLevel.AtLeastOnce, caCert);
+        }
+
+        void Connect_to_the_mqtt_broker() 
+        {
+            //might have to take into account network config, time, and availability here!
+            _awsMqttClient.Open();
+        }
+
+        void Send_a_telemetry_message_to_the_broker() { }
+
+        void Receive_a_client_message_from_the_broker() { }
+
+        void Get_the_unnamed_shadow() { }
+
+        void Update_the_unnamed_shadow() { }
+
+        void Delete_the_unnamed_shadow() { }
+
+        void Handle_the_delta_unnamed_shadow() { }
+
+        void Cleanup() 
+        {
+            _awsMqttClient.Close();
+        }
+    }
+}
