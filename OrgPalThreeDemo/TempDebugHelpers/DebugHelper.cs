792e853d9767e83a51435f20e2be3ab915c0ca11
Robin Jones <robin@networkfusion.co.uk>
2021-10-08 16:38:09 +0100

    Use sortable pattern for datetime (which is closest to ISO8601).

--- OrgPalThreeDemo/TempDebugHelpers/DebugHelper.cs
+++ OrgPalThreeDemo/TempDebugHelpers/DebugHelper.cs
@@ -24,7 +24,7 @@ namespace OrgPalThreeDemo.TempDebugHelpers
                     if (key.ToString() == "timestamp")
                     {
                         Debug.Write(GetSpaces(level));
-                        Debug.WriteLine($"as ISO date: {DateTime.FromUnixTimeSeconds((int)hash[key]).ToString("yyyy-MM-ddTHH:mm:ssZ")}"); //TODO: should work with "o", but doesnt!
+                        Debug.WriteLine($"as ISO date: {DateTime.FromUnixTimeSeconds((int)hash[key]).ToString("s")}Z"); //TODO: should work with "o", but doesnt!
                     }
                 }
             }
