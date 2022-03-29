4fbfc60db838acc388220273c3b6ed73763d3cc3
Robin Jones <robin@networkfusion.co.uk>
2022-03-29 01:39:03 +0100

    Use properties in MqttConnector.

--- OrgPalThreeDemo/AwsIotCore/MqttConnector.cs
+++ OrgPalThreeDemo/AwsIotCore/MqttConnector.cs
@@ -4,14 +4,14 @@ namespace OrgPalThreeDemo.AwsIotCore
 {
     public static class MqttConnector
     {
-        public static string ConnectionMethod = string.Empty; //TODO: for future, could be WebSocket or MQTT
-        public static string Host = string.Empty; //make sure to add your AWS endpoint and region, e.g. "<endpoint>-ats.iot.<region>.amazonaws.com"
-        public static int Port = 8883; //add your port if different from the default e.g. 8883 // TODO: not going to change really!
-        public static string ThingName = string.Empty; //add your "thing"
+        public static string ConnectionMethod { get; set; } = string.Empty; //TODO: for future, could be WebSocket or MQTT
+        public static string Host { get; set; } = string.Empty; //make sure to add your AWS endpoint and region, e.g. "<endpoint>-ats.iot.<region>.amazonaws.com"
+        public static int Port { get; set; } = 8883; //add your port if different from the default e.g. 8883 // TODO: not going to change really!
+        public static string ThingName { get; set; } = string.Empty; //add your "thing"
 
-        public static string ClientRsaSha256Crt = string.Empty; //Device Certificate copied from AWS
-        public static string ClientRsaKey = string.Empty; //Device private key copied from AWS
-        public static byte[] RootCA; //AWS root CA
-        public static MqttConnectionClient Client;
+        public static string ClientRsaSha256Crt { get; set; } = string.Empty; //Device Certificate copied from AWS
+        public static string ClientRsaKey { get; set; } = string.Empty; //Device private key copied from AWS
+        public static byte[] RootCA { get; set; } //AWS root CA
+        public static MqttConnectionClient Client { get; set; }
     }
 }
