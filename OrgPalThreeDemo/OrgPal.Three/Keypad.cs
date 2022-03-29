e9909a716f693fb047190e00ca39da751f7af526
Robin Jones <robin@networkfusion.co.uk>
2022-03-29 01:44:26 +0100

    Fix keypad dispose.

--- OrgPalThreeDemo/OrgPal.Three/Keypad.cs
+++ OrgPalThreeDemo/OrgPal.Three/Keypad.cs
@@ -5,6 +5,7 @@ namespace OrgPal.Three
 {
     public class Keypad : IDisposable
     {
+        private bool _disposed;
         readonly GpioPin _keypadPin1;
         readonly GpioPin _keypadPin2;
         readonly GpioPin _keypadPin3;
@@ -20,12 +21,30 @@ namespace OrgPal.Three
 
         }
 
-        public void Dispose()
+        /// <summary>
+        /// Releases unmanaged resources
+        /// and optionally release managed resources
+        /// </summary>
+        /// <param name="disposing"><see langword="true" /> to release both managed and unmanaged resources; <see langword="false" /> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
         {
+            if (_disposed) return;
+
             _keypadPin1.Dispose();
             _keypadPin2.Dispose();
             _keypadPin3.Dispose();
             _keypadPin4.Dispose();
+
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                Dispose(true);
+                _disposed = true;
+            }
+            GC.SuppressFinalize(this);
         }
     }
 }
