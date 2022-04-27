4b92c4c8b6dd5dcca27efe269c20848b3465dc55
Robin Jones <robin@networkfusion.co.uk>
2022-04-27 19:45:32 +0100

    Update select nugets

--- OrgPalThreeDemo/Program.cs
+++ OrgPalThreeDemo/Program.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 /*
-    This program targets (and is tested against) firmware  ORGPAL_PALTHREE-1.8.0.56
+    This program targets (and is tested against) firmware  ORGPAL_PALTHREE-1.8.0.72
     `nanoff --masserase --update --target ORGPAL_PALTHREE`
     Future firmware (or nuget updates) might break it!!!
 
