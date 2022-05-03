f15ed4d60d2f15c9c9ddf7b54e32866a4995ef61
Robin Jones <robin@networkfusion.co.uk>
2022-05-03 15:43:41 +0100

    Update FW version (fixes issue with collections)

--- OrgPalThreeDemo/Program.cs
+++ OrgPalThreeDemo/Program.cs
@@ -2,8 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 /*
-    This program targets (and is tested against) firmware  ORGPAL_PALTHREE-1.8.0.72
-    `nanoff --masserase --update --target ORGPAL_PALTHREE --fwversion 1.8.0.72`
+    This program targets (and is tested against) firmware  ORGPAL_PALTHREE-1.8.0.80
+    `nanoff --masserase --update --target ORGPAL_PALTHREE --fwversion 1.8.0.80`
     Future firmware (or nuget updates) might break it!!!
 
     Known Issues:
