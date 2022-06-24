22f9f80148580899d0aedda396825a43a775201e
Robin Jones <networkfusion@users.noreply.github.com>
2022-06-24 23:58:15 +0100

    Further work on HMP155

--- OrgPalThreeDemo/Peripherals/VaisalaHMP155.cs
+++ OrgPalThreeDemo/Peripherals/VaisalaHMP155.cs
@@ -13,15 +13,14 @@ namespace OrgPalThreeDemo.Peripherals
     /// </remarks>
     public class VaisalaHMP155 : IDisposable
     {
-        SerialPortRS485 sensor = new SerialPortRS485();
-        int probeAddress;
+        readonly SerialPortRS485 sensor;
+        public int probeAddress { get; set; } = 0;
 
-        public VaisalaHMP155(int connector = 0, int busAddress = 0, int baudrate = 4800)
+        public VaisalaHMP155(int connector = 0)
         {
-            probeAddress = busAddress;
             sensor = new SerialPortRS485(connector);
 
-            sensor.Port.BaudRate = baudrate;
+            sensor.Port.BaudRate = 4800;
             sensor.Port.DataBits = 7;
             sensor.Port.Parity = Parity.Even;
             sensor.Port.StopBits = StopBits.One;
@@ -31,16 +30,15 @@ namespace OrgPalThreeDemo.Peripherals
 
         public void Close()
         {
-            sensor.Port.DataReceived -= Port_DataReceived;
             if (sensor.Port.IsOpen)
             {
                 sensor.Port.Close();
             }
+            sensor.Port.DataReceived -= Port_DataReceived;
         }
 
         public void Open()
         {
-            sensor.Port.DataReceived += Port_DataReceived;
             if (sensor.Port.IsOpen)
             {
                 sensor.Port.Close();
@@ -49,6 +47,21 @@ namespace OrgPalThreeDemo.Peripherals
             Debug.WriteLine("HMP155 serial port opened!");
 
             //TODO: support poll mode?
+
+            sensor.Port.WriteLine("smode stop"); //or is this just "S"
+            sensor.Port.WriteLine("?"); //Get the device info
+            while (sensor.Port.BytesToRead > 0)
+            {
+                Debug.WriteLine(sensor.Port.ReadLine());
+            }
+            //sensor.Port.WriteLine("open");
+            sensor.Port.WriteLine("intv 15 s");
+            Debug.WriteLine(sensor.Port.ReadLine());
+            sensor.Port.WriteLine("smode run"); // or should it just be "R"
+
+            // We should possibily disable the transmit line now (unless we are in poll mode)?!
+
+            sensor.Port.DataReceived += Port_DataReceived;
         }
 
         private void Port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
@@ -57,20 +70,21 @@ namespace OrgPalThreeDemo.Peripherals
             DecodeMessage(serialDevice.ReadLine().TrimEnd(new char[] {'\r','\n' }));
         }
 
-        void DecodeMessage(string message)
+        private void DecodeMessage(string message)
         {
             //TODO: handle message. (default format).
             // "RH= 33.0 %RH T= 22.1 'C"
             Debug.WriteLine(message);
+            if (message.StartsWith("RH="))
+            {
+                // starts with "RH=" and ends with "%RH", strip those chars, then convert to a double?!
+                var humidity = message.Substring(2, 6).Trim(' '); // Temporary "workaround?!
+                                                                  // starts with "T=" and ends with "'C", strip those chars, then convert to a double?!
+                var temperature = message.Substring(14, 6).Trim(' '); // Temporary "workaround?!
+                Debug.WriteLine($"RH:{humidity}, T:{temperature}");
+            }
         }
 
-        void SendCommand()
-        {
-            //R (set temporary run)
-            //S (end run mode)
-            //INTV (the interval to send data)
-            //...
-        }
 
         public void Dispose()
         {
